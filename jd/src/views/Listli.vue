b682ae8cab119abf3739294bff3f53e6e728d9fc
entingzhang <591692578@qq.com>
2018-12-11 21:35:32 +0800

    分类页面头部搜索框的添加

--- jd/src/views/Listli.vue
+++ jd/src/views/Listli.vue
@@ -1,15 +1,17 @@
 <template>
-	<div>
-		<h2>分类</h2>
+	<div class="listli">
+		<search-bar></search-bar>
 		<dibu></dibu>
 	</div>
 
 </template>
 
 <script>
+	import SearchBar from "@/components/Search.vue";
 	import Dibu from "@/components/Footer.vue";
 	export default {
 		components: {
+			SearchBar,
 			Dibu
 		}
 	}
